113729d0c8b2e57dcc4112c7a41411ae925d5e44
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-06-16 11:30:44 +0300

    [FT] Add lot of features

--- models.py
+++ models.py
@@ -10,21 +10,19 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(30), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime(), default=datetime.utcnow)
-    is_confirm = db.Column(db.Boolean(), default = 0)
-
-class Realtor(db.Model, UserMixin):
-    __tablename__ = 'Realtors'
-    id = db.Column(db.Integer, primary_key = True)
-    email = db.Column(db.String(30), nullable=False, unique=True)
-    password = db.Column(db.String(255), nullable=False)
-    is_admin = db.Column(db.Boolean(), default = 1)
-
+    is_admin = db.Column(db.Boolean(), default=0)
 class House(db.Model):
-    __tablename__ = 'Houses'
-    id = db.Column(db.Integer(), primary_key = True)
-    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True, nullable=True)
-    construction_year = db.Column(db.Integer(), default = 0)
-    location = db.Column(db.String(255), default = "")
-    house_square = db.Column(db.Numeric())
-    furniture_filling = db.Column(db.String(300))
-    price = db.Column(db.Numeric(12,2))
+    __tablename__ = 'Properties'
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column('user_id', db.Integer, db.ForeignKey('Users.id'))
+    name = db.Column(db.String(19), nullable=False)
+    location = db.Column(db.String(255), nullable=False)
+    house_square = db.Column(db.Float())
+    rooms = db.Column(db.String(300))
+    price = db.Column(db.Float())
+    image_filename = db.Column(db.String(255), nullable = False)
+
+class Favorites(db.Model):
+    __tablename__= 'Favorites'
+    user_id = db.Column('user_id',db.Integer, db.ForeignKey('Users.id'), primary_key=True)
+    favorite_subject = db.Column(db.Integer, db.ForeignKey('Properties.id'))
