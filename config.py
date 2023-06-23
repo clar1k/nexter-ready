32e52f9e6229cec1d272a59b06ff5e9c226dba29
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-06-23 16:28:56 +0300

    Add default config fields

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 from os import environ as env
 from dotenv import load_dotenv
 load_dotenv('.env')
-class Config:
-    SECRET_KEY = env.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI=env.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS=env.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+class envConfig:
     SECRET_KEY=env.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI=env.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER=env.get('MAIL_SERVER')
     MAIL_PORT=env.get('MAIL_PORT')
     MAIL_USE_SSL=env.get('MAIL_USE_SSL')
@@ -14,3 +12,10 @@ class Config:
     PASSWORD_PEPPER=env.get('PASSWORD_PEPPER')
     MAIL_DEFAULT_SENDER=env.get('MAIL_DEFAULT_SENDER')
     UPLOAD_FOLDER=env.get('UPLOAD_FOLDER')
+
+
+class Config:
+    SECRET_KEY="EHFNAISOJF@!*Y*(G*@!(EG*(@EY)))"
+    SQLALCHEMY_DATABASE_URI="sqlite:///realtor.db"
+    PASSWORD_PEPPER="a912j30mv0912he"
+    UPLOAD_FOLDER="static/uploads"
