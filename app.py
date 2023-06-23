6e0ee2a3ed10d2381d79ff44d985b153291f41d3
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-06-23 15:44:26 +0300

    Add password hashing, salt, pepper, email confirm.
    Add email confirm feature.
    Add generating a salt to user when signin up.
    Add PASSWORD_PEPPER static variable to a config.

--- app.py
+++ app.py
@@ -1,28 +1,30 @@
 import os
 from dotenv import load_dotenv
+from decorators import admin_required, email_confirmed
 from os import environ as env
 from flask import Flask, abort, flash, redirect, render_template, request, session, url_for
 from flask_login import LoginManager, login_required, login_user
-from models import User, Property, db, Favorites, Image
+from models import db ,User, Property, Like, Image
 from werkzeug.utils import secure_filename
 from flask_mail import Mail, Message
+from werkzeug.security import gen_salt, check_password_hash, generate_password_hash
+from itsdangerous import URLSafeSerializer
+from config import Config
+
+#! Undeployed
+#TODO: Use flask flash messages.
+#TODO: Sign in with Google, Facebook feature.
+
+#? Unnecessary :D
+#TODO: Change db to a PostgreSQL
 
 app = Flask(__name__)
-
-load_dotenv('.env')
-app.config['SECRET_KEY'] = env.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = env.get('SQLALCHEMY_DATABASE_URI')
-app.config['MAIL_SERVER'] = env.get('MAIL_SERVER')
-app.config['MAIL_PORT'] = env.get('MAIL_PORT')
-app.config['MAIL_USE_SSL'] = env.get('MAIL_USE_SSL')
-app.config['MAIL_USERNAME'] = env.get('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = env.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = env.get('MAIL_DEFAULT_SENDER')
-app.config['UPLOAD_FOLDER'] = env.get('UPLOAD_FOLDER') 
+app.config.from_object(Config)
 
 db.init_app(app)
 login_manager = LoginManager(app)
 mail = Mail(app)
+serializer = URLSafeSerializer(app.config['SECRET_KEY'])
 
 
 @login_manager.user_loader
@@ -45,17 +47,16 @@ def index():
 
 @app.route('/login', methods = ['POST'])
 def login():
-    if request.method == 'POST':
-        try:
-            user = User.query.filter_by(email=request.form.get('email')).first()
-            if user is not None and request.form.get('password') == user.password:
-                login_user(user)
-                session['user_id'] = user.id
-                return redirect('/')
-        except Exception as e:
-            return str(e)
-    else:
-        return abort(status=404)
+    user = User.query.filter_by(email=request.form.get('email')).first()
+    password_salt = user.salt
+    password_pepper = app.config['PASSWORD_PEPPER']
+    form_password = request.form.get('password') + password_salt + password_pepper
+    if user and check_password_hash(user.password,form_password) and user.is_confirm:
+        login_user(user)
+        session['user_id'] = user.id
+        return redirect('/')
+    else: 
+        return abort(401)
 
 
 @app.route('/logout', methods = ['GET'])
@@ -66,23 +67,50 @@ def logout():
 
 @app.route('/register', methods = ['POST'])
 def register():
-    if request.method == 'POST':
-        new_user = User(
-            fullname=request.form.get('fullname'),
-            email=request.form.get('email'),
-            password = request.form.get('password')
-        )
-        db.session.add(new_user)
-        db.session.commit()
-        return redirect('/')
+    salt = gen_salt(16)
+    password_pepper = app.config['PASSWORD_PEPPER']
+    new_user = User(
+        fullname=request.form.get('fullname'),
+        email=request.form.get('email'),
+        salt=salt,
+        password=generate_password_hash(request.form.get('password') + salt + password_pepper)
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    send_confirm_msg(new_user.email)
+    login_user(new_user)
+    return redirect('/')
 
+
+@app.route('/confirm/<token>')
+def confirm_email(token):
+    email = serializer.loads(token,'email_confirm', max_age=100)
+    user = User.query.filter_by(email=email).first()
+    user.is_confirm = 1
+    db.session.commit()
+    return redirect('/')
+
+
+def generate_token(email: str):
+    return serializer.dumps(email,'email_confirm')
+
+
+def send_confirm_msg(email: str) -> None:
+    token = generate_token(email)
+    msg = Message(
+        subject="Confirm your email",
+        html=f'Press the <a href="http://127.0.0.1:5000/confirm/{token}"target="_blank"> link </a> to confirm your email' ,
+        recipients=[email],
+        sender=('Nexter Ready',app.config['MAIL_DEFAULT_SENDER']),
+    )
+    mail.send(msg)
 
 
 def send_to_realtor(realtor_mail=["test@example.com"], user_email='test@example.com'):
     msg = Message(subject=f'Please contact me',
         body=f"I am interested in your property \n My email: {user_email} \n\n",
         recipients=realtor_mail,
-        sender=app.config['MAIL_DEFAULT_SENDER'],
+        sender=('Nexter Ready',app.config['MAIL_DEFAULT_SENDER']),
         )
     mail.send(msg)
 
@@ -98,10 +126,8 @@ def contact_realtor(id: int):
 
 @app.route('/add-property', methods=['GET'])
 @login_required
+@admin_required
 def add_property():
-    user = User.query.get(session.get('user_id'))
-    if not user.is_admin:
-        return abort(403)
     return render_template('newProperty.html') 
 
 
@@ -111,7 +137,7 @@ def add_filename(filename: str, property_id: int):
         db.session.commit()
 
 
-def save_images(images , property_id: int): 
+def save_images(images: list, property_id: int): 
     for image in images:
         filename = secure_filename(image.filename)
         add_filename(image.filename, property_id)
@@ -119,11 +145,12 @@ def save_images(images , property_id: int):
 
 
 @app.route('/add',methods=['POST'])
+@admin_required
 def add_prop():
     if request.form.get('action') == 'submit':
         new_property = Property(
             name=request.form.get('name'),
-            user_id=session.get('user_id'),
+            author_id=session.get('user_id'),
             rooms=request.form.get('rooms'),
             location=request.form.get('location'),
             price=request.form.get('price'),
@@ -152,21 +179,24 @@ def property(id: int):
     )
 
 
-@app.route('/add-to-favorites/<int:id>', methods=['PUT'])
+@app.route('/add-to-favorites/<int:id>', methods=['GET'])
 def add_to_favorites(id: int):
-    favorite = Favorites.query.filter_by(user_id=session.get('user_id'), favorite_property=id).first()
+    favorite = Like.query.filter_by(user_id=session.get('user_id'), favorite_property=id).first()
     if favorite:
         db.session.delete(favorite)
         db.session.commit()        
         return '', 204
     else:
-        favorite = Favorites(user_id=session.get('user_id'), favorite_property=id)
+        favorite = Like(user_id=session.get('user_id'), favorite_property=id)
         db.session.add(favorite)
         db.session.commit()
         return '', 204
 
 
-if __name__ == '__main__':
+def create_db():
     with app.app_context():
         db.create_all()
+
+
+if __name__ == '__main__':
     app.run(debug=True)
