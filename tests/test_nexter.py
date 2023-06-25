8218520c207e9e66c8ecc768db47de4f64df90fa
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-06-25 17:00:48 +0300

    Add new tests for add_property

--- tests/test_nexter.py
+++ tests/test_nexter.py
@@ -1,13 +1,15 @@
 import pytest
 import os
+import io
+import tempfile
 from app import app
 from app import create_db
-from models import User, Property, Like
+from models import User, Property, Like, Image as db_image
 from flask import session
 from models import db
-import io
 from PIL import Image
-
+from app import generate_token
+from werkzeug.datastructures import FileStorage
 
 @pytest.fixture
 def client():
@@ -18,29 +20,32 @@ def client():
 
 def test_index_page(client):
     response = client.get('/')
-    assert response.status_code == 201
+    assert response.status_code == 200
     assert b"The ultimate personal freedom" in response.data
 
 
 def test_registration(client):
     response = client.post('/register', 
-    data = {
+    data = 
+    {
         "fullname":"Test User",
         "email":"test@test.com",
         "password":"test_password",
-        "is_confirm":"1",
-        "is_admin":"1"
     })
+    confirm_mail = client.get(f'/confirm/{generate_token("test@test.com")}')
     with app.app_context():
         assert User.query.count() == 1
         user = User.query.filter_by(email="test@test.com").first()
         assert user.email == 'test@test.com'
-        assert response.status_code == 400
+        assert response.status_code == 201
+        assert user.is_confirm == 1
+        
 
 
 def test_login(client):
     response = client.post('/login',
-        data = {
+        data = 
+        {
             "email":"test@test.com",
             "password":"test_password",
         }
@@ -51,19 +56,24 @@ def test_login(client):
 
 def test_add_property(client):
     test_login(client)
-    response = client.post('/add', data={
-        'name': 'Example Property',
-        'rooms': 3,
-        'location': 'Test Location',
-        'price': 100000,
-        'area': 200,
-        "action":"submit"
-    })
+    make_admin = client.get('/make-me-admin')
+    response = client.post('/add', 
+        data = {
+            'name': 'Example Property',
+            'rooms': 3,
+            'location': 'Test Location',
+            'price': 100000,
+            'area': 200,
+            "action":"submit"
+        }
+    )
     with app.app_context():
-        prop = Property.query.filter_by(name='Example Property',rooms=3).first()
+        assert b"The values for name and location shoul be unique" in response.data
+        assert response.status_code == 400
+        prop = Property.query.filter_by(name='Example Property', rooms=3).first()
         assert Property.query.count() == 1
         assert prop.name == "Example Property"
-        assert response.status_code == 201
+
 
 def test_like(client):
     test_login(client)
